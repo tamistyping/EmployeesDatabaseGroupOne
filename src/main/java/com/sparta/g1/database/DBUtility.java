79439a979495aecea78753f89596b2b5b2d68060
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 14:54:06 +0100

    fix: fix data types and return type of executePreparedStatement

--- src/main/java/com/sparta/g1/database/DBUtility.java
+++ src/main/java/com/sparta/g1/database/DBUtility.java
@@ -3,7 +3,6 @@ package com.sparta.g1.database;
 import com.sparta.g1.EmployeeDataCleaner;
 
 import java.sql.*;
-import java.util.List;
 import java.util.Set;
 
 public class DBUtility {
@@ -12,30 +11,30 @@ public class DBUtility {
         return statement.executeQuery(query);
     }
 
-    public static ResultSet executePreparedStatement(Connection connection, String query, Object... params) throws SQLException {
+    public static void executePreparedStatement(Connection connection, String query, Object... params) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         for (int i = 0; i < params.length; i++) {
             preparedStatement.setString(i + 1, (String) params[i]);
         }
-        return preparedStatement.executeQuery();
+        preparedStatement.executeUpdate();
     }
 
     public static void insertEmployeeIntoDatabase(Set<String> employees) {
-        String insertEmployeeSQL = "INSERT INTO Employee (EmpID, NamePrefix, FirstName, MiddleInitial, LastName, Gender, Email, DateOfBirth, DateOfJoining, Salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String insertEmployeeSQL = "INSERT INTO EmployeeData (EmpID, NamePrefix, FirstName, MiddleInitial, LastName, Gender, Email, DateOfBirth, DateOfJoining, Salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try (Connection connection = DBConnection.getInstance().getConnection()) {
             for (String employee: employees) {
                 String[] fields = employee.split(",");
                 try {
-                    int empId = (int) EmployeeDataCleaner.convertToDataType(fields[0], "int");
+                    String empId = EmployeeDataCleaner.convertToDataType(fields[0], "int").toString();
                     String namePrefix = fields[1];
                     String firstName = fields[2];
-                    char middleInitial = (char) EmployeeDataCleaner.convertToDataType(fields[3], "char");
+                    String middleInitial = EmployeeDataCleaner.convertToDataType(fields[3], "char").toString();
                     String lastName = fields[4];
-                    char gender = (char) EmployeeDataCleaner.convertToDataType(fields[5], "char");
+                    String gender = EmployeeDataCleaner.convertToDataType(fields[5], "char").toString();
                     String email = fields[6];
-                    Date dateOfBirth = (Date) EmployeeDataCleaner.convertToDataType(fields[7], "date");
-                    Date dateOfJoining = (Date) EmployeeDataCleaner.convertToDataType(fields[8], "date");
-                    int salary = (int) EmployeeDataCleaner.convertToDataType(fields[9], "int");
+                    String dateOfBirth = EmployeeDataCleaner.convertToDataType(fields[7], "date").toString();
+                    String dateOfJoining = EmployeeDataCleaner.convertToDataType(fields[8], "date").toString();
+                    String salary = EmployeeDataCleaner.convertToDataType(fields[9], "int").toString();
 
                     executePreparedStatement(connection, insertEmployeeSQL, empId, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
                 } catch (Exception e) {
