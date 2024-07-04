c32216784e87007893b1cf694130f32b6e73687b
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 16:25:08 +0100

    fix: correct table name

--- src/main/java/com/sparta/g1/DatabaseQueries.java
+++ src/main/java/com/sparta/g1/DatabaseQueries.java
@@ -1,17 +1,17 @@
 package com.sparta.g1;
 
 public interface DatabaseQueries {
-    String ID_SEARCH = "SELECT * FROM employees WHERE employeeID = ?";
-    String NAME_SEARCH = "SELECT * FROM employees WHERE lastName LIKE ?";
-    String GENDER_SEARCH = "SELECT * FROM employees WHERE gender = ?";
-    String MIN_SALARY = "SELECT * FROM employees WHERE salary > ?";
-    String MAX_SALARY = "SELECT * FROM employees WHERE salary < ?";
-    String ALL_EMPLOYEES = "SELECT * FROM employees";
-    String INSERT_EMPLOYEE = "INSERT INTO employees (employeeID, prefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    String UPDATE_EMPLOYEE = "UPDATE employees SET prefix = ?, firstName = ?, middleInitial = ?, lastName = ?, gender = ?, email = ?, dateOfBirth = ?, dateOfJoining = ?, salary = ? WHERE employeeID = ?";
-    String DELETE_EMPLOYEE = "DELETE FROM employees WHERE employeeID = ?";
-    String COUNT_EMPLOYEES = "SELECT COUNT(*) FROM employees";
-    String AVERAGE_SALARY = "SELECT AVG(salary) FROM employees";
-    String EMPLOYEES_JOINED_AFTER = "SELECT * FROM employees WHERE dateOfJoining > ?";
-    String EMPLOYEES_BORN_BEFORE = "SELECT * FROM employees WHERE dateOfBirth < ?";
+    String ID_SEARCH = "SELECT * FROM EmployeeData WHERE empId = ?";
+    String NAME_SEARCH = "SELECT * FROM EmployeeData WHERE lastName LIKE ?";
+    String GENDER_SEARCH = "SELECT * FROM EmployeeData WHERE gender = ?";
+    String MIN_SALARY = "SELECT * FROM EmployeeData WHERE salary > ?";
+    String MAX_SALARY = "SELECT * FROM EmployeeData WHERE salary < ?";
+    String ALL_EMPLOYEES = "SELECT * FROM EmployeeData";
+    String INSERT_EMPLOYEE = "INSERT INTO EmployeeData (employeeID, prefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    String UPDATE_EMPLOYEE = "UPDATE EmployeeData SET prefix = ?, firstName = ?, middleInitial = ?, lastName = ?, gender = ?, email = ?, dateOfBirth = ?, dateOfJoining = ?, salary = ? WHERE employeeID = ?";
+    String DELETE_EMPLOYEE = "DELETE FROM EmployeeData WHERE employeeID = ?";
+    String COUNT_EMPLOYEES = "SELECT COUNT(*) FROM EmployeeData";
+    String AVERAGE_SALARY = "SELECT AVG(salary) FROM EmployeeData";
+    String EMPLOYEES_JOINED_AFTER = "SELECT * FROM EmployeeData WHERE dateOfJoining > ?";
+    String EMPLOYEES_BORN_BEFORE = "SELECT * FROM EmployeeData WHERE dateOfBirth < ?";
 }
