7ab457591caca003f52f1b3e63016ec41fce7716
Tam <enquiretam@gmail.com>
2024-07-04 10:51:34 +0100

    Converted to hashset - final

--- src/main/java/com/sparta/g1/EmployeeFactory.java
+++ src/main/java/com/sparta/g1/EmployeeFactory.java
@@ -4,23 +4,16 @@ import java.util.*;
 
 public class EmployeeFactory {
 
-        public static String[] getEmployees(int numEmployees) throws IOException {
-            if (numEmployees < 1 || numEmployees > 1000)
-                throw new IllegalArgumentException("Argument 'numEmployees' must be between 1 and 1000");
-            List<String> result = new ArrayList<>();
+    public static Set<String> getEmployees() throws IOException {
+        Set<String> result = new HashSet<>();
 
-            try (BufferedReader f = new BufferedReader(new FileReader("EmployeesProjectG1/src/main/resources/employees.csv"))) {
-                String employeeLine;
-                while ((employeeLine = f.readLine()) != null)
-                    result.add(employeeLine);
+        try (BufferedReader f = new BufferedReader(new FileReader("EmployeesProjectG1/src/main/resources/employees.csv"))) {
+            String employeeLine;
+            while ((employeeLine = f.readLine()) != null) {
+                result.add(employeeLine);
             }
-
-            if (result.size() < numEmployees) {
-                throw new IllegalArgumentException("The file does not contain enough records.");
-            }
-
-            Collections.shuffle(result);
-            return result.subList(0, numEmployees).toArray(new String[0]);
         }
-}
 
+        return result;
+    }
+}
