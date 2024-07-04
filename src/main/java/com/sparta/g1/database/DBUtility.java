096544676403936a64365ce135e5ead503e6c837
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 15:38:40 +0100

    fix: Add connection to method parameters

--- src/main/java/com/sparta/g1/database/DBUtility.java
+++ src/main/java/com/sparta/g1/database/DBUtility.java
@@ -19,13 +19,45 @@ public class DBUtility {
         preparedStatement.executeUpdate();
     }
 
-    public static void insertEmployeeIntoDatabase(Set<String> employees) {
+    public static void dropTable() {
+        String insertDropTableSQL = "DROP TABLE IF EXISTS EmployeeData;";
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            executePreparedStatement(connection, insertDropTableSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createEmployeeTable() {
+        String insertCreateTableSQL =
+                ("CREATE TABLE EmployeeData (\n" +
+                "    EmpID INT PRIMARY KEY,\n" +
+                "    NamePrefix VARCHAR(10),\n" +
+                "    FirstName VARCHAR(50),\n" +
+                "    MiddleInitial CHAR(1),\n" +
+                "    LastName VARCHAR(50),\n" +
+                "    Gender CHAR(1),\n" +
+                "    Email VARCHAR(100),\n" +
+                "    DateOfBirth DATE,\n" +
+                "    DateOfJoining DATE,\n" +
+                "    Salary INT\n" +
+                ");");
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            executePreparedStatement(connection, insertCreateTableSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void insertEmployeeIntoDatabase(Connection connection, Set<String> employees) {
         String insertEmployeeSQL = "INSERT INTO EmployeeData (EmpID, NamePrefix, FirstName, MiddleInitial, LastName, Gender, Email, DateOfBirth, DateOfJoining, Salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        try (Connection connection = DBConnection.getInstance().getConnection()) {
             for (String employee: employees) {
                 String[] fields = employee.split(",");
                 try {
-                    String empId = EmployeeDataCleaner.convertToDataType(fields[0], "int").toString();
+                    String empId = fields[0];
                     String namePrefix = fields[1];
                     String firstName = fields[2];
                     String middleInitial = EmployeeDataCleaner.convertToDataType(fields[3], "char").toString();
@@ -34,16 +66,14 @@ public class DBUtility {
                     String email = fields[6];
                     String dateOfBirth = EmployeeDataCleaner.convertToDataType(fields[7], "date").toString();
                     String dateOfJoining = EmployeeDataCleaner.convertToDataType(fields[8], "date").toString();
-                    String salary = EmployeeDataCleaner.convertToDataType(fields[9], "int").toString();
+                    String salary = fields[9];
 
                     executePreparedStatement(connection, insertEmployeeSQL, empId, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
 
     }
 }
