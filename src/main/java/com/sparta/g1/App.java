8e72db1c17208240d683376b93d0ae4282cca204
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 15:40:32 +0100

    refactor: remove redundant imports

--- src/main/java/com/sparta/g1/App.java
+++ src/main/java/com/sparta/g1/App.java
@@ -4,9 +4,6 @@ import com.sparta.g1.database.DBConnection;
 import com.sparta.g1.database.DBUtility;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class App {
     public static Connection connection = DBConnection.getInstance().getConnection();
