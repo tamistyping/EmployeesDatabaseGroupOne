7eccc9d8c059cf04253efb30db6f83d46def09fa
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 17:25:25 +0100

    test: delete employee call in main

--- src/main/java/com/sparta/g1/App.java
+++ src/main/java/com/sparta/g1/App.java
@@ -5,21 +5,36 @@ import com.sparta.g1.database.DBUtility;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class App {
     public static Connection connection = DBConnection.getInstance().getConnection();
 
     public static void main(String[] args) {
-        DBUtility.dropTable();
-        DBUtility.createEmployeeTable();
-        DBUtility.insertEmployeeIntoDatabase(connection, EmployeeFactory.getEmployees());
+//        DBUtility.dropTable();
+//        DBUtility.createEmployeeTable();
+//        DBUtility.insertEmployeeIntoDatabase(connection, EmployeeFactory.getEmployees());
 
-        try {
-            ResultSet query = DBUtility.executePreparedStatementQuery(connection, DatabaseQueries.ID_SEARCH, "121372");
-            DBUtility.printResultSet(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+
+        String query = DatabaseQueries.DELETE_EMPLOYEE;
+
+        String empId = "744722";
+        String namePrefix = "Dr.";
+        String firstName = "Henry";
+        String middleInitial = "H";
+        String lastName = "McFarlane";
+        String gender = "F";
+        String email = "bibi.lewis@yahoo.co.in";
+        String dateOfBirth = "1988-01-01";
+        String dateOfJoining = "2015-01-01";
+        String salary = "100000";
+
+
+//        DBUtility.executePreparedStatementUpdate(connection, query, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary, empId);
+        DBUtility.executePreparedStatementUpdate(connection, query, empId);
+
+
 
 
         DBConnection.getInstance().closeConnection();
