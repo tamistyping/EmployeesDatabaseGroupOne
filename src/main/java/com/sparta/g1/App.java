25ef837710a6b44cd40f6f52c7bf27b9c90f9810
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 21:53:44 +0100

    test: test getEmployeeByName

--- src/main/java/com/sparta/g1/App.java
+++ src/main/java/com/sparta/g1/App.java
@@ -1,24 +1,17 @@
 package com.sparta.g1;
 
 import com.sparta.g1.database.DBConnection;
+import com.sparta.g1.database.DBQuery;
 import com.sparta.g1.database.DBUtility;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class App {
     public static Connection connection = DBConnection.getInstance().getConnection();
 
     public static void main(String[] args) {
         DBUtility.tableInit(connection);
-        try {
-            ResultSet query = DBUtility.executePreparedStatementQuery(
-                    connection, DatabaseQueries.NAME_SEARCH, "Cl%");
-            DBUtility.printResultSet(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        DBQuery.getEmployeeByName("ren");
+
         DBConnection.getInstance().closeConnection();
     }
 }
