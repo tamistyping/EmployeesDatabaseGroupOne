6cb2f15824257a937564e08484d9fc1815fe5575
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 14:55:42 +0100

    test: Insert clean employee data to employee database

--- src/main/java/com/sparta/g1/App.java
+++ src/main/java/com/sparta/g1/App.java
@@ -1,6 +1,12 @@
 package com.sparta.g1;
 
+import com.sparta.g1.database.DBConnection;
 import com.sparta.g1.database.DBUtility;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class App {
     public static void main(String[] args) {
