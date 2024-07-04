e1229ddacfc017761af580eb73896da5569d6723
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 17:24:33 +0100

    fix: correct column names and and equal to for salaries

--- src/main/java/com/sparta/g1/DatabaseQueries.java
+++ src/main/java/com/sparta/g1/DatabaseQueries.java
@@ -4,12 +4,12 @@ public interface DatabaseQueries {
     String ID_SEARCH = "SELECT * FROM EmployeeData WHERE empId = ?";
     String NAME_SEARCH = "SELECT * FROM EmployeeData WHERE lastName LIKE ?";
     String GENDER_SEARCH = "SELECT * FROM EmployeeData WHERE gender = ?";
-    String MIN_SALARY = "SELECT * FROM EmployeeData WHERE salary > ?";
-    String MAX_SALARY = "SELECT * FROM EmployeeData WHERE salary < ?";
+    String MIN_SALARY = "SELECT * FROM EmployeeData WHERE salary >= ?";
+    String MAX_SALARY = "SELECT * FROM EmployeeData WHERE salary <= ?";
     String ALL_EMPLOYEES = "SELECT * FROM EmployeeData";
-    String INSERT_EMPLOYEE = "INSERT INTO EmployeeData (employeeID, prefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    String UPDATE_EMPLOYEE = "UPDATE EmployeeData SET prefix = ?, firstName = ?, middleInitial = ?, lastName = ?, gender = ?, email = ?, dateOfBirth = ?, dateOfJoining = ?, salary = ? WHERE employeeID = ?";
-    String DELETE_EMPLOYEE = "DELETE FROM EmployeeData WHERE employeeID = ?";
+    String INSERT_EMPLOYEE = "INSERT INTO EmployeeData (empId, NamePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    String UPDATE_EMPLOYEE = "UPDATE EmployeeData SET NamePrefix = ?, firstName = ?, middleInitial = ?, lastName = ?, gender = ?, email = ?, dateOfBirth = ?, dateOfJoining = ?, salary = ? WHERE empId = ?";
+    String DELETE_EMPLOYEE = "DELETE FROM EmployeeData WHERE empId = ?";
     String COUNT_EMPLOYEES = "SELECT COUNT(*) FROM EmployeeData";
     String AVERAGE_SALARY = "SELECT AVG(salary) FROM EmployeeData";
     String EMPLOYEES_JOINED_AFTER = "SELECT * FROM EmployeeData WHERE dateOfJoining > ?";
