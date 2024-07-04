60cb9ea941aed924b1269569d275c815801d8839
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 16:25:31 +0100

    test: Find employee by ID

--- src/main/java/com/sparta/g1/App.java
+++ src/main/java/com/sparta/g1/App.java
@@ -2,8 +2,9 @@ package com.sparta.g1;
 
 import com.sparta.g1.database.DBConnection;
 import com.sparta.g1.database.DBUtility;
-
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class App {
     public static Connection connection = DBConnection.getInstance().getConnection();
@@ -12,6 +13,15 @@ public class App {
         DBUtility.dropTable();
         DBUtility.createEmployeeTable();
         DBUtility.insertEmployeeIntoDatabase(connection, EmployeeFactory.getEmployees());
+
+        try {
+            ResultSet query = DBUtility.executePreparedStatementQuery(connection, DatabaseQueries.ID_SEARCH, "121372");
+            DBUtility.printResultSet(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         DBConnection.getInstance().closeConnection();
     }
 }
