3a6334aa33b458bbe0ee60948a6717843f5903c2
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-07-04 16:24:06 +0100

    feat: Add get query method and change exception handling

--- src/main/java/com/sparta/g1/database/DBUtility.java
+++ src/main/java/com/sparta/g1/database/DBUtility.java
@@ -3,6 +3,7 @@ package com.sparta.g1.database;
 import com.sparta.g1.EmployeeDataCleaner;
 
 import java.sql.*;
+import java.util.List;
 import java.util.Set;
 
 public class DBUtility {
@@ -11,22 +12,41 @@ public class DBUtility {
         return statement.executeQuery(query);
     }
 
-    public static void executePreparedStatement(Connection connection, String query, Object... params) throws SQLException {
+    public static void executePreparedStatementUpdate(Connection connection, String query, Object... params) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < params.length; i++) {
+            try {
+                assert preparedStatement != null;
+                preparedStatement.setString(i + 1, (String) params[i]);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            assert preparedStatement != null;
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet executePreparedStatementQuery(Connection connection, String query, String... params) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         for (int i = 0; i < params.length; i++) {
-            preparedStatement.setString(i + 1, (String) params[i]);
+            preparedStatement.setString(i + 1, params[i]);
         }
-        preparedStatement.executeUpdate();
+        return preparedStatement.executeQuery();
     }
 
     public static void dropTable() {
         String insertDropTableSQL = "DROP TABLE IF EXISTS EmployeeData;";
         Connection connection = DBConnection.getInstance().getConnection();
-        try {
-            executePreparedStatement(connection, insertDropTableSQL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executePreparedStatementUpdate(connection, insertDropTableSQL);
     }
 
     public static void createEmployeeTable() {
@@ -44,11 +64,7 @@ public class DBUtility {
                 "    Salary INT\n" +
                 ");");
         Connection connection = DBConnection.getInstance().getConnection();
-        try {
-            executePreparedStatement(connection, insertCreateTableSQL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executePreparedStatementUpdate(connection, insertCreateTableSQL);
 
     }
 
@@ -68,12 +84,34 @@ public class DBUtility {
                     String dateOfJoining = EmployeeDataCleaner.convertToDataType(fields[8], "date").toString();
                     String salary = fields[9];
 
-                    executePreparedStatement(connection, insertEmployeeSQL, empId, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
+                    executePreparedStatementUpdate(connection, insertEmployeeSQL, empId, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
 
 
+    }
+
+
+    public static void printResultSet(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        for (int i = 1; i <= columnCount; i++) {
+            if (i > 1) System.out.print(" || ");
+            System.out.print(metaData.getColumnLabel(i));
+        }
+        System.out.println();
+        System.out.println();
+
+        while (resultSet.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                if (i > 1) System.out.print(" || ");
+                System.out.print(resultSet.getString(i));
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
